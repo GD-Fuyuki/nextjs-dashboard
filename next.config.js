e3e16781de631394842703f3f2e04578e096c375
EC2 Default User <ec2-user@ip-172-31-32-136.ap-northeast-1.compute.internal>
2024-05-17 08:10:25 +0000

    17

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["c9f69013330940ca95b9aaa8deaa286f.vfs.cloud9.ap-northeast-1.amazonaws.com"],
+      // allowedForwardedHosts: ["localhost:3000"],
+      // ^ You might have to use this property depending on your exact version.
+    }
+  }
+};
 
 module.exports = nextConfig;
